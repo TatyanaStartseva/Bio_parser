45e086a863a709794451436a993df67158d8e8a8
Startseva Tatyana <TatyanaStartseva2020@yandex.ru>
2024-09-07 16:17:29 +0300

    Выборка из БД по возрастанию значения dateUpdated

--- bio_parser.py
+++ bio_parser.py
@@ -1,3 +1,4 @@
+import datetime
 import io
 import logging
 import os
@@ -13,7 +14,7 @@ from aiohttp import ClientTimeout, ClientProxyConnectionError
 from aiogram import Bot
 from db.db import db
 from pymongo import ASCENDING
-
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
@@ -113,18 +114,28 @@ async def process_users():
             host, port, user, password = line.strip().split(":")
             proxy.append(f"http://{user}:{password}@{host}:{port}")
     try:
-        with open("size_offset.txt", "r") as f:
-            size, offset = map(int, (f.read()).split())
-        j = 0
         users_ids = await load_users_ids()
-        for user_id in users_ids:
-            await bot.send_message(
-                user_id, f"Начинаю обработку пользователей с {offset} по {offset+size}"
-            )
         pool = db()
         cursor_users = pool["users"]
-        cursor = cursor_users.find().sort("dateUpdated", ASCENDING).limit(size)
+        size = 15000
+        cursor = cursor_users.find({}).sort({"dateUpdated": 1}).limit(size)
         documents = await cursor.to_list(length=size)
+        if documents:
+            min_date = (
+                documents[0]["dateUpdated"].replace(microsecond=0)
+                if "dateUpdated" in documents[0]
+                else datetime.now()
+            )
+            max_date = (
+                documents[-1]["dateUpdated"].replace(microsecond=0)
+                if "dateUpdated" in documents[-1]
+                else datetime.now()
+            )
+        for user_id in users_ids:
+            await bot.send_message(
+                user_id,
+                f"Начинаю обработку пользователей с {min_date} по {max_date}",
+            )
         usernames = [
             {
                 "username": record["username"],
@@ -162,9 +173,9 @@ async def process_users():
                     if len(name_parts) == 2:
                         last_name = name_parts[1]
                 if bio is not None and response["bio"] is not None:
-                    if bio.lower().replace(" ", "") != response["bio"].lower().replace(
-                        " ", ""
-                    ):
+                    if str(bio).lower().replace(" ", "") != str(
+                        response["bio"]
+                    ).lower().replace(" ", ""):
                         update_operation = cursor_users.update_one(
                             {"user_id": user_id},
                             {
@@ -173,11 +184,11 @@ async def process_users():
                                     "bio": response["bio"],
                                     "first_name": first_name,
                                     "last_name": last_name,
+                                    "dateUpdated": datetime.now(),
                                 }
                             },
                         )
                         updates.append(update_operation)
-                        j += 1
                         ws.append(
                             [
                                 f"{username}",
@@ -188,6 +199,15 @@ async def process_users():
                             ]
                         )
                     else:
+                        update_operation = cursor_users.update_one(
+                            {"user_id": user_id},
+                            {
+                                "$set": {
+                                    "dateUpdated": datetime.now(),
+                                }
+                            },
+                        )
+                        updates.append(update_operation)
                         ws.append(
                             [
                                 f"{username}",
@@ -206,11 +226,11 @@ async def process_users():
                                 "bio": response["bio"],
                                 "first_name": first_name,
                                 "last_name": last_name,
+                                "dateUpdated": datetime.now(),
                             }
                         },
                     )
                     updates.append(update_operation)
-                    j += 1
                     ws.append(
                         [
                             f"{username}",
@@ -221,6 +241,15 @@ async def process_users():
                         ]
                     )
                 elif response["bio"] is None:
+                    update_operation = cursor_users.update_one(
+                        {"user_id": user_id},
+                        {
+                            "$set": {
+                                "dateUpdated": datetime.now(),
+                            }
+                        },
+                    )
+                    updates.append(update_operation)
                     ws.append(
                         [
                             f"{username}",
@@ -239,11 +268,11 @@ async def process_users():
                                 "bio": response["bio"],
                                 "first_name": first_name,
                                 "last_name": last_name,
+                                "dateUpdated": datetime.now(),
                             }
                         },
                     )
                     updates.append(update_operation)
-                    j += 1
                     ws.append(
                         [
                             f"{username}",
@@ -256,14 +285,19 @@ async def process_users():
                 access_request += 1
             else:
                 ban_values.append((user_id,))
-                j += 1
                 fail_request += 1
                 ws.append([f"{username}", "None", f"{bio}", "True", "True"])
-        await asyncio.gather(*updates)
         if ban_values:
-            cursor_users.update_many(
-                {"user_id": {"$in": ban_values}}, {"$set": {"ban": True}}
+            await cursor_users.update_many(
+                {"user_id": {"$in": [user_id for user_id, in ban_values]}},
+                {
+                    "$set": {
+                        "ban": True,
+                        "dateUpdated": datetime.now(),
+                    }
+                },
             )
+        await asyncio.gather(*updates)
         wb.save("info_parse_bio.xlsx")
         with open("info_parse_bio.xlsx", "rb") as f:
             file_content = f.read()
@@ -273,7 +307,9 @@ async def process_users():
                 byte_file_content.seek(0)
                 await bot.send_message(
                     user_id,
-                    f"Обработано {size} пользователей. \n С {offset} по {offset + size} \n Успешные запросы: {access_request}, \n Заблокированные пользователи: {fail_request} \n Кол-во обновленией в БД: {j}",
+                    f"Обработано {size} пользователей. \n С {min_date} по "
+                    f"{max_date} \n Успешные запросы: {access_request}, "
+                    f"\n Заблокированные пользователи: {fail_request} ",
                 )
                 await bot.send_document(user_id, byte_file_content)
     except Exception as e:
